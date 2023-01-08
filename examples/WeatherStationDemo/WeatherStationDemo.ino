ae4bfa6b917e799032e943a8622e79b0d2272cbd
John <john@noreply.com>
2023-01-07 19:09:12 -0800

    Public version of functioning WeatherStationDemo1.ino

--- examples/WeatherStationDemo/WeatherStationDemo.ino
+++ examples/WeatherStationDemo/WeatherStationDemo.ino
@@ -33,6 +33,7 @@ See more at https://thingpulse.com
 #include <time.h>                       // time() ctime()
 #include <sys/time.h>                   // struct timeval
 #include <coredecls.h>                  // settimeofday_cb()
+#include <TZ.h>                         // see example code: NTP-TZ-DST.ino (v2)
 
 #include "SSD1306Wire.h"
 #include "OLEDDisplayUi.h"
@@ -42,17 +43,66 @@ See more at https://thingpulse.com
 #include "WeatherStationFonts.h"
 #include "WeatherStationImages.h"
 
+// Set the controlling #defines here
+#define EXTENDED_SENSORS
+#define EXTENDED_UPLOAD
+
+#ifdef EXTENDED_SENSORS
+#include <Adafruit_BMP085.h>
+#endif
 
 /***************************
  * Begin Settings
  **************************/
 
+// Site-specific settings - change these according to your location and API keys
+// =============================================================================
+
 // WIFI
 const char* WIFI_SSID = "yourssid";
 const char* WIFI_PWD = "yourpassw0rd";
 
-#define TZ              2       // (utc+) TZ in hours
-#define DST_MN          60      // use 60mn for summer time in some countries
+#define LOCAL_TZ TZ_America_Los_Angeles
+
+// OpenWeatherMap Settings
+// Sign up here to get an API key:
+// https://docs.thingpulse.com/how-tos/openweathermap-key/
+const String OPEN_WEATHER_MAP_APP_ID = "XXX";
+/*
+Go to https://openweathermap.org/find?q= and search for a location. Go through the
+result set and select the entry closest to the actual location you want to display
+data for. It'll be a URL like https://openweathermap.org/city/2657896. The number
+at the end is what you assign to the constant below.
+ */
+const String OPEN_WEATHER_MAP_LOCATION_ID = "5359777";    // Irvine, CA, USA
+
+// Pick a language code from this list:
+// Arabic - ar, Bulgarian - bg, Catalan - ca, Czech - cz, German - de, Greek - el,
+// English - en, Persian (Farsi) - fa, Finnish - fi, French - fr, Galician - gl,
+// Croatian - hr, Hungarian - hu, Italian - it, Japanese - ja, Korean - kr,
+// Latvian - la, Lithuanian - lt, Macedonian - mk, Dutch - nl, Polish - pl,
+// Portuguese - pt, Romanian - ro, Russian - ru, Swedish - se, Slovak - sk,
+// Slovenian - sl, Spanish - es, Turkish - tr, Ukrainian - ua, Vietnamese - vi,
+// Chinese Simplified - zh_cn, Chinese Traditional - zh_tw.
+const String OPEN_WEATHER_MAP_LANGUAGE = "en";
+const uint8_t MAX_FORECASTS = 4;
+
+const boolean IS_METRIC = true;
+
+// thingspeak settings
+#ifdef EXTENDED_UPLOAD
+const char *host = "api.thingspeak.com";                  // URL of the thingspeak server
+const char *api_key ="YourThingSpeakWriteAPIKey";
+const int httpPort = 80;
+#endif
+
+// Adjust according to your language
+const String WDAY_NAMES[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+const String MONTH_NAMES[] = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+
+
+// Hardware-related settings
+// =========================
 
 // Setup
 const int UPDATE_INTERVAL_SECS = 20 * 60; // Update every 20 minutes
@@ -67,60 +117,41 @@ const int SDA_PIN = 5; //D3;
 const int SDC_PIN = 4; //D4;
 #endif
 
+// Sensor settings
+#ifdef EXTENDED_SENSORS
+// DHT11 Settings
+#define DHT11_PIN 14       // ESP8266-12E  D5 read Temperature and Humidity data
+int temp = 0; //temperature
+int humi = 0; //humidity
 
-// OpenWeatherMap Settings
-// Sign up here to get an API key:
-// https://docs.thingpulse.com/how-tos/openweathermap-key/
-String OPEN_WEATHER_MAP_APP_ID = "XXX";
-/*
-Go to https://openweathermap.org/find?q= and search for a location. Go through the
-result set and select the entry closest to the actual location you want to display 
-data for. It'll be a URL like https://openweathermap.org/city/2657896. The number
-at the end is what you assign to the constant below.
- */
-String OPEN_WEATHER_MAP_LOCATION_ID = "2657896";
+// Light and Atmospheric Pressure sensor Settings
+const int Light_ADDR = 0b0100011;   // BH1750FVI at I2C address:0x23
 
-// Pick a language code from this list:
-// Arabic - ar, Bulgarian - bg, Catalan - ca, Czech - cz, German - de, Greek - el,
-// English - en, Persian (Farsi) - fa, Finnish - fi, French - fr, Galician - gl,
-// Croatian - hr, Hungarian - hu, Italian - it, Japanese - ja, Korean - kr,
-// Latvian - la, Lithuanian - lt, Macedonian - mk, Dutch - nl, Polish - pl,
-// Portuguese - pt, Romanian - ro, Russian - ru, Swedish - se, Slovak - sk,
-// Slovenian - sl, Spanish - es, Turkish - tr, Ukrainian - ua, Vietnamese - vi,
-// Chinese Simplified - zh_cn, Chinese Traditional - zh_tw.
-String OPEN_WEATHER_MAP_LANGUAGE = "de";
-const uint8_t MAX_FORECASTS = 4;
+Adafruit_BMP085 bmp;
+const int Atm_ADDR  = 0b1110111;    // BMP085/BMP180 sensor at I2C address:0x77
 
-const boolean IS_METRIC = true;
-
-// Adjust according to your language
-const String WDAY_NAMES[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-const String MONTH_NAMES[] = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+int tempLight = 0;
+int tempAtm = 0;
+#endif
 
 /***************************
  * End Settings
  **************************/
- // Initialize the oled display for address 0x3c
- // sda-pin=14 and sdc-pin=12
- SSD1306Wire     display(I2C_DISPLAY_ADDRESS, SDA_PIN, SDC_PIN);
- OLEDDisplayUi   ui( &display );
 
+// Initialize the OLED display I2C address
+SSD1306Wire     display(I2C_DISPLAY_ADDRESS, SDA_PIN, SDC_PIN);
+OLEDDisplayUi   ui( &display );
+
+WiFiClient client;
 OpenWeatherMapCurrentData currentWeather;
 OpenWeatherMapCurrent currentWeatherClient;
 
 OpenWeatherMapForecastData forecasts[MAX_FORECASTS];
 OpenWeatherMapForecast forecastClient;
 
-#define TZ_MN           ((TZ)*60)
-#define TZ_SEC          ((TZ)*3600)
-#define DST_SEC         ((DST_MN)*60)
-time_t now;
-
 // flag changed in the ticker function every 10 minutes
 bool readyForWeatherUpdate = false;
-
 String lastUpdate = "--";
-
 long timeSinceLastWUpdate = 0;
 
 //declaring prototypes
@@ -133,22 +164,52 @@ void drawForecastDetails(OLEDDisplay *display, int x, int y, int dayIndex);
 void drawHeaderOverlay(OLEDDisplay *display, OLEDDisplayUiState* state);
 void setReadyForWeatherUpdate();
 
+#ifdef EXTENDED_SENSORS
+void readTemperatureHumidity();
+void readLight();
+void readAtmosphere();
+
+#ifdef EXTENDED_UPLOAD
+void uploadTemperatureHumidity();
+#endif
+
+#endif
 
 // Add frames
 // this array keeps function pointers to all frames
 // frames are the single views that slide from right to left
 FrameCallback frames[] = { drawDateTime, drawCurrentWeather, drawForecast };
-int numberOfFrames = 3;
+const int numberOfFrames = 3;
 
 OverlayCallback overlays[] = { drawHeaderOverlay };
-int numberOfOverlays = 1;
+const int numberOfOverlays = 1;
+
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println();
 
-  // initialize dispaly
+#ifdef EXTENDED_SENSORS
+  // Initialize sensors on I2C bus
+  Wire.begin(0,2);
+
+  Wire.beginTransmission(Atm_ADDR);
+  // initialize BMP085/BMP180 Atmospheric Pressure sensor
+  if (!bmp.begin()) {
+    Serial.println("Could not find BMP180 or BMP085 sensor at Atm_ADDR");
+  }else{
+    Serial.println("Initialized BMP180 or BMP085 sensor");
+  }
+  Wire.endTransmission();
+
+  // initialize light sensor
+  Wire.beginTransmission(Light_ADDR);
+  Wire.write(0b00000001);
+  Wire.endTransmission();
+#endif
+
+  // initialize OLED display
   display.init();
   display.clear();
   display.display();
@@ -173,8 +234,9 @@ void setup() {
 
     counter++;
   }
+
   // Get time from network time service
-  configTime(TZ_SEC, DST_SEC, "pool.ntp.org");
+  configTime(LOCAL_TZ, "pool.ntp.org");
 
   ui.setTargetFPS(30);
 
@@ -191,21 +253,39 @@ void setup() {
   // You can change the transition that is used
   // SLIDE_LEFT, SLIDE_RIGHT, SLIDE_TOP, SLIDE_DOWN
   ui.setFrameAnimation(SLIDE_LEFT);
-
   ui.setFrames(frames, numberOfFrames);
-
   ui.setOverlays(overlays, numberOfOverlays);
 
-  // Inital UI takes care of initalising the display too.
+  // Initial UI takes care of initializing the display too.
   ui.init();
 
   Serial.println("");
 
   updateData(&display);
-
 }
 
 void loop() {
+
+#ifdef EXTENDED_SENSORS
+  static long readTime = 0;
+  static long uploadTime = 0;
+
+  //Read sensors every 5 seconds
+  if(millis() - readTime > 5000){
+    readTemperatureHumidity();
+    readLight();
+    readAtmosphere();
+    readTime = millis();
+  }
+
+#ifdef EXTENDED_UPLOAD
+  //Upload Temperature Humidity every 60 seconds
+  if(millis() - uploadTime > 60000){
+    uploadTemperatureHumidity();
+    uploadTime = millis();
+  }
+#endif
+#endif
 
   if (millis() - timeSinceLastWUpdate > (1000L*UPDATE_INTERVAL_SECS)) {
     setReadyForWeatherUpdate();
@@ -217,15 +297,12 @@ void loop() {
   }
 
   int remainingTimeBudget = ui.update();
-
   if (remainingTimeBudget > 0) {
     // You can do some work here
     // Don't do stuff if you are below your
     // time budget.
     delay(remainingTimeBudget);
   }
-
-
 }
 
 void drawProgress(OLEDDisplay *display, int percentage, String label) {
@@ -255,23 +332,20 @@ void updateData(OLEDDisplay *display) {
   delay(1000);
 }
 
-
-
 void drawDateTime(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  time_t now;
   now = time(nullptr);
   struct tm* timeInfo;
   timeInfo = localtime(&now);
   char buff[16];
 
-
   display->setTextAlignment(TEXT_ALIGN_CENTER);
   display->setFont(ArialMT_Plain_10);
   String date = WDAY_NAMES[timeInfo->tm_wday];
-
   sprintf_P(buff, PSTR("%s, %02d/%02d/%04d"), WDAY_NAMES[timeInfo->tm_wday].c_str(), timeInfo->tm_mday, timeInfo->tm_mon+1, timeInfo->tm_year + 1900);
   display->drawString(64 + x, 5 + y, String(buff));
-  display->setFont(ArialMT_Plain_24);
 
+  display->setFont(ArialMT_Plain_24);
   sprintf_P(buff, PSTR("%02d:%02d:%02d"), timeInfo->tm_hour, timeInfo->tm_min, timeInfo->tm_sec);
   display->drawString(64 + x, 15 + y, String(buff));
   display->setTextAlignment(TEXT_ALIGN_LEFT);
@@ -303,6 +377,7 @@ void drawForecastDetails(OLEDDisplay *display, int x, int y, int dayIndex) {
   time_t observationTimestamp = forecasts[dayIndex].observationTime;
   struct tm* timeInfo;
   timeInfo = localtime(&observationTimestamp);
+
   display->setTextAlignment(TEXT_ALIGN_CENTER);
   display->setFont(ArialMT_Plain_10);
   display->drawString(x + 20, y, WDAY_NAMES[timeInfo->tm_wday]);
@@ -316,16 +391,18 @@ void drawForecastDetails(OLEDDisplay *display, int x, int y, int dayIndex) {
 }
 
 void drawHeaderOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
+  time_t now;
   now = time(nullptr);
   struct tm* timeInfo;
   timeInfo = localtime(&now);
   char buff[14];
-  sprintf_P(buff, PSTR("%02d:%02d"), timeInfo->tm_hour, timeInfo->tm_min);
 
   display->setColor(WHITE);
   display->setFont(ArialMT_Plain_10);
   display->setTextAlignment(TEXT_ALIGN_LEFT);
+  sprintf_P(buff, PSTR("%02d:%02d"), timeInfo->tm_hour, timeInfo->tm_min);
   display->drawString(0, 54, String(buff));
+
   display->setTextAlignment(TEXT_ALIGN_RIGHT);
   String temp = String(currentWeather.temp, 1) + (IS_METRIC ? "°C" : "°F");
   display->drawString(128, 54, temp);
@@ -336,3 +413,129 @@ void setReadyForWeatherUpdate() {
   Serial.println("Setting readyForUpdate to true");
   readyForWeatherUpdate = true;
 }
+
+
+#ifdef EXTENDED_SENSORS
+//read temperature & humidity data - DHT11 sensor; digital serial interface via pin D5
+void readTemperatureHumidity(){
+  unsigned int loopCnt;
+  int chr[40] = {0};
+  unsigned long time1;
+  int tempfrac;
+
+bgn:
+  delay(2000);
+
+  //Set interface mode 2 to: output
+  //Output low level 20ms (>18ms)
+  //Output high level 40μs
+  pinMode(DHT11_PIN, OUTPUT);
+  digitalWrite(DHT11_PIN, LOW);
+  delay(20);
+  digitalWrite(DHT11_PIN, HIGH);
+  delayMicroseconds(40);
+  digitalWrite(DHT11_PIN, LOW);
+
+  //Set interface mode 2: input
+  pinMode(DHT11_PIN, INPUT);
+
+  //High level response signal
+  loopCnt = 10000;
+  while (digitalRead(DHT11_PIN) != HIGH){
+    if (loopCnt-- == 0){
+      //If don't return to high level for a long time, output a prompt and start over
+      Serial.println("HIGH");
+      goto bgn;
+    }
+  }
+
+  //Low level response signal
+  loopCnt = 30000;
+  while (digitalRead(DHT11_PIN) != LOW){
+    if (loopCnt-- == 0){
+      //If don't return low for a long time, output a prompt and start over
+      Serial.println("LOW");
+      goto bgn;
+    }
+  }
+
+  //Start reading the value of bit1-40
+  for (int i = 0; i < 40; i++){
+    while (digitalRead(DHT11_PIN) == LOW){}
+
+    //When the high level occurs, write down the time "time"
+    time1 = micros();
+    while (digitalRead(DHT11_PIN) == HIGH){}
+
+    //When there is a low level, write down the time and subtract the time just saved
+    //If the value obtained is greater than 50μs, it is ‘1’, otherwise it is ‘0’
+    //And save it in an array
+    if (micros() - time1  > 50){
+      chr[i] = 1;
+    } else {
+      chr[i] = 0;
+    }
+  }
+
+  //Humidity, 8-bit bit, converted to a value
+  humi = chr[0] * 128 + chr[1] * 64 + chr[2] * 32 + chr[3] * 16 + chr[4] * 8 + chr[5] * 4 + chr[6] * 2 + chr[7];
+  //Temperature, 8-bit bit, converted to a value
+  temp = chr[16] * 128 + chr[17] * 64 + chr[18] * 32 + chr[19] * 16 + chr[20] * 8 + chr[21] * 4 + chr[22] * 2 + chr[23];
+  tempfrac = chr[24] * 128 + chr[25] * 64 + chr[26] * 32 + chr[27] * 16 + chr[28] * 8 + chr[29] * 4 + chr[30] * 2 + chr[31];
+
+  Serial.print("temp:");
+  Serial.print(temp);
+  Serial.print(tempfrac);
+  Serial.print("    humi:");
+  Serial.println(humi);
+}
+
+// read BH1750FVI light sensor (I2C interface)
+void readLight(){
+  // reset
+  Wire.beginTransmission(Light_ADDR);
+  Wire.write(0b00000111);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(Light_ADDR);
+  Wire.write(0b00100000);
+  Wire.endTransmission();
+
+  // typical read delay 120ms
+  delay(120);
+  Wire.requestFrom(Light_ADDR, 2); // 2byte every time
+  for (tempLight = 0; Wire.available() >= 1; ) {
+    char c = Wire.read();
+    tempLight = (tempLight << 8) + (c & 0xFF);
+  }
+  tempLight = tempLight / 1.2;
+  Serial.print("light: ");
+  Serial.println(tempLight);
+}
+
+// read atmospheric pressure from BMP085/BMP180 sensor (I2C interface)
+void readAtmosphere(){
+  tempAtm = bmp.readPressure();
+  Serial.print("Pressure = ");
+  Serial.print(tempAtm);
+  Serial.println(" Pascal");
+}
+#endif
+
+#ifdef EXTENDED_UPLOAD
+//upload temperature, humidity, light & atmos pressure data to thingspeak.com
+void uploadTemperatureHumidity(){
+   if(!client.connect(host, httpPort)){
+    Serial.println("connection failed");
+    return;
+  }
+
+  // Four values (field1 field2 field3 field4) have been set in thingspeak.com
+  client.print(String("GET ") + "/update?api_key="+api_key+"&field1="+temp+"&field2="+humi + "&field3="+tempLight+"&field4="+tempAtm+" HTTP/1.1\r\n" +"Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+}
+#endif
